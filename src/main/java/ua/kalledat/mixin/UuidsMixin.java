6593f7e7bf7a64a3386097e4695f56be65eeea4c
Yevhenii Smyrnov <kalledat57@gmail.com>
2025-10-18 15:57:10 +0300

    refactor mixin usage

--- src/main/java/ua/kalledat/mixin/UuidsMixin.java
+++ src/main/java/ua/kalledat/mixin/UuidsMixin.java
@@ -1,27 +1,22 @@
 package ua.kalledat.mixin;
 
 import net.minecraft.util.Uuids;
-import org.apache.commons.lang3.StringUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import ua.kalledat.PlayerMigration;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Optional;
-import java.util.UUID;
 import java.util.function.Function;
 
 @Mixin(Uuids.class)
 public class UuidsMixin {
 
-    @Redirect(method = "getOfflinePlayerUuid", at = @At(value = "INVOKE", target = "Ljava/util/UUID;nameUUIDFromBytes([B)Ljava/util/UUID;"))
-    private static UUID test(byte[] md) {
-        var nickname = StringUtils.substringAfter(new String(md), "OfflinePlayer:");
+    @ModifyVariable(method = "getOfflinePlayerUuid", at = @At(value = "HEAD"), argsOnly = true)
+    private static String modifyNickname(String nickname) {
         return Optional.ofNullable(PlayerMigration.playerMigrationRepo)
                 .map(repo -> repo.getOriginalNickname(nickname))
                 .flatMap(Function.identity())
-                .map(oldNickname -> UUID.nameUUIDFromBytes(("OfflinePlayer:" + oldNickname).getBytes(StandardCharsets.UTF_8)))
-                .orElse(UUID.nameUUIDFromBytes(md));
+                .orElse(nickname);
     }
 }
